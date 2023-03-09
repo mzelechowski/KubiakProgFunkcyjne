e4557d1a08a2a35ad219cef4fcf580f056c37073
maciek zelek <mzelechowski79@gmail.com>
2023-03-09 17:27:34 +0100

    usuniecie warningow

--- src/Zadanie01k1.java
+++ src/Zadanie01k1.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,13 +20,14 @@ public class Zadanie01k1 {
                 listLenghtMin5.add(i);
             }
         }
-        System.out.println("Imperatywne - Ilosc imion które mają 5 i wiecej liter wynosi: "+licznik);
+        System.out.println("Imperatywne - Ilosc imion które mają 5 i wiecej znaków wynosi: "+licznik);
             System.out.println("oraz oto te imona: " +listLenghtMin5);
 
         //Rozwiązanie funkcyjne
+        System.out.println();
         int count = (int) imiona.stream().filter(i ->i.length()>=5).count();
         List<String> listFunc = imiona.stream().filter(i ->i.length()>=5).collect(Collectors.toList());
-        System.out.println("Funkcyjne - Ilosc imion które mają 5 i wiecej liter wynosi: "+count);
+        System.out.println("Funkcyjne - Ilosc imion które mają 5 i wiecej znaków wynosi: "+count);
         System.out.println("oraz oto te imona: " +listFunc);
     }
 }
