69eb4343395fb13a817238475e94fec4a8153cbd
maciek zelek <mzelechowski79@gmail.com>
2023-03-09 18:17:42 +0100

    zadanie 2.1

---
+++ src/Zadanie02k1.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Zadanie02k1 {
+    public static void main(String[] args) {
+        /*Napisz program który jednowymiarowej wpisuje liczby od 1 do 100
+        i następnie je sumuje
+         */
+        int n=100, suma=0;
+        int [] tab=new int[n];
+        for (int i = 1; i <=n ; i++) {
+            tab[i-1]=i;
+        }
+        for (int t:tab) {
+            suma+=t;
+        }
+        System.out.println("Suma elemntow z tablicy 1-100 wynosi: "+suma);
+
+        //funkcynie
+        int sumAll = Arrays.stream(tab).sum();
+        System.out.println("Suma elemntow z tablicy 1-100 wynosi: "+sumAll);
+    }
+}
---
+++ src/Zadanie02k2.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Zadanie02k2 {
+    public static void main(String[] args) {
+        int n=100, sumP=0, sumNP=0;
+        int [] tab=new int[n];
+        for (int i = 1; i <=n ; i++) {
+            tab[i-1]=i;
+        }
+        for (int t:tab) {
+            if(t%2==0)
+                sumP+=t;
+            else
+                sumNP+=t;
+        }
+        System.out.println("Suma elemntow PARZYSTYCH z tablicy 1-100 wynosi: "+sumP);
+        System.out.println("Suma elemntow NIEPARZYSTYCH z tablicy 1-100 wynosi: "+sumNP);
+
+        List<Integer> lista = Arrays.stream(tab).boxed().collect(Collectors.toList());
+        int sumPF=lista.stream().filter(l->l%2==0).collect(Collectors.summingInt(l->l));
+        System.out.println("Suma elemntow PARZYSTYCH sumPF z tablicy 1-100 wynosi: "+sumPF);
+        int sumNPF=lista.stream().filter(l->l%2!=0).collect(Collectors.summingInt(l->l));
+        System.out.println("Suma elemntow PARZYSTYCH sumNPF z tablicy 1-100 wynosi: "+sumNPF);
+
+    }
+}
