6a98b240fd9df9cc4812a797c599aa5edf09d309
maciek zelek <mzelechowski79@gmail.com>
2023-03-09 19:55:53 +0100

    zadanie 2.3

--- src/Zadanie02k2.java
+++ src/Zadanie02k2.java
@@ -1,9 +1,6 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Zadanie02k2 {
     public static void main(String[] args) {
---
+++ src/Zadanie02k3.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Zadanie02k3 {
+    public static void wpisz(List list){
+        for (int i = 0; i < list.size(); i++) {
+            if(i < list.size()-1)
+                System.out.print(list.get(i)+" , ");
+            else
+                System.out.print(list.get(i));
+        }
+    }
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < 6; i++) {
+            list.add((int)Math.floor(Math.random()*101));
+        }
+        System.out.println("Elementy nieposortowane");
+        Zadanie02k3.wpisz(list);
+        System.out.println();
+        Collections.sort(list);
+        System.out.println("Elementy posortowane");
+        Zadanie02k3.wpisz(list);
+        list.remove(1);
+        list.add(10);
+        System.out.println();
+        Collections.sort(list);
+        Zadanie02k3.wpisz(list);
+
+    }
+}
