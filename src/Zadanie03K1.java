784f25aa89e23db3efeb350e0716acfc86b7e428
maciek zelek <mzelechowski79@gmail.com>
2023-03-11 14:00:37 +0100

    Funkcje wyzszego rzedu zadanie 3.1 oraz zadanie 3.1

---
+++ src/Zadanie03K1.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Zadanie03K1 {
+    public static void main(String[] args) {
+        /*Npisz program który sortuje listę Stringów przy pomocy
+        metody compareTo
+         */
+        List<String> lista = new ArrayList<>();
+        lista.add("Ewelina");
+        lista.add("Maciek");
+        lista.add("Ania");
+        lista.add("Piotrek");
+        lista.add("Barbara");
+        System.out.println("Lista przed sortowaniem");
+        lista.stream().forEach(l -> System.out.print(l + ", "));
+        Collections.sort(lista, (String a, String b) ->
+        {
+            return a.compareTo(b);
+        });
+        System.out.println("Lista po sotowaniu.");
+        System.out.println(lista);
+    }
+}
---
+++ src/Zadanie03K2.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Zadanie03K2 {
+    public static void main(String[] args) {
+    /*Napisz program który listę z zadania 03K2 sortuje w kolejnosci
+    odwrotnej
+     */
+        List<String> lista = new ArrayList<>();
+        lista.add("Maciek");
+        lista.add("Lolek");
+        lista.add("Anna");
+        lista.add("Genowefa");
+        lista.add("Waclaw");
+        System.out.println("Lista w wersji oryginalnej");
+        lista.forEach(l -> System.out.print(l + ", "));
+        Collections.sort(lista, (String a, String b) -> {
+            return b.compareTo(a);
+        });
+        System.out.println();
+        lista.forEach(l-> System.out.print(l+", "));
+    }
+}
